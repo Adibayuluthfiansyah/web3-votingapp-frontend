6bc878b59557cac5fa31d2259c3780cc227b0b08
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-12-10 09:18:29 +0700

    make logic for render data from backend

---
+++ app/constant/index.ts
@@ -0,0 +1,5 @@
+import votingABI from "../abi/VotingApp.json";
+
+export const CONTRACT_ADDRESS = process.env.ENV_PROCESS_CONTRACT_ADDRESS!;
+export const CONTRACT_ABI = votingABI.abi;
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "./constant/index";
+import { useState, useEffect } from "react";
+
+interface Proposal {
+  author: string;
+  name: string;
+  votecount: number;
+}
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1>WELCOME TO VOTING APP WEB3!</h1>
-    </div>
-  );
+  const [account, setAccount] = useState<string>("");
+  const [proposals, setProposals] = useState<Proposal[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [contract, setContract] = useState<ethers.Contract | null>(null);
+  const [name, setName] = useState<string>("");
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      setAccount(address);
+      const votingContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        CONTRACT_ABI,
+        signer
+      );
+      setContract(votingContract);
+    }
+
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1>WELCOME TO VOTING APP WEB3!</h1>
+      </div>
+    );
+  };
 }
